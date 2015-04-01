63ededf822ca9005fedc0fdf313dcf039de4ca7a
glaucogoncalves <glaestgon@gmail.com>
2015-04-01 15:31:49 -0300

    Update pratica-2-II.py
    Ajeitando para o original

--- pratica-2-II.py
+++ pratica-2-II.py
@@ -12,56 +12,34 @@ from mininet.util import irange,dumpNodeConnections
 net = None
 
 def createTopo():
-        topo=Topo()
+	topo=Topo()
 
         swCore1 = topo.addSwitch('s1')
-        swCore2 = topo.addSwitch('s2')
-        swCore3 = topo.addSwitch('s3')
-        swCore4 = topo.addSwitch('s4')
 
-        ## Ajuste do parametro de fanout da rede
-        fanout1  = 2
-	fanout2  = 8 
+	## Ajuste do parametro de fanout da rede
+	fanout = 2
 
         # Switches counter
-        lastSW = 5
+        lastSW = 2
         lastHost = 1
 
         # Aggregation switches loop
-        for i in irange (1, fanout2):
+        for i in irange (1, fanout):
                 swAggregL = topo.addSwitch('s%s' % lastSW)
-                
-		if i % 2 == 0:
-			topo.addLink(swCore3, swAggregL)
-                	topo.addLink(swCore4, swAggregL)
-                else:
-			topo.addLink(swCore1, swAggregL)
-                	topo.addLink(swCore2, swAggregL)
+                topo.addLink(swCore1, swAggregL)
                 lastSW += 1
 
                 # Edge switches loop
-                for j in irange (1, fanout1):
-			                               				                
-    		                	        
-			if i % 2 == 1:
-				swEdge = topo.addSwitch('s%s' % lastSW)
-	
-				topo.addLink(swAggregL, swEdge)
-	                	lastSW += 1
-			else:
-				if j % 2 == 1:
-#					swEdge2 = topo.addSwitch('s%s' % (lastSW-1))
-					topo.addLink(swAggregL, swEdge)
-				else:
-					swEdge2 = topo.addSwitch('s%s' % (lastSW-3))
-					topo.addLink(swAggregL, swEdge2)
+                for j in irange (1, fanout):
+                        swEdge = topo.addSwitch('s%s' % lastSW)
+                        topo.addLink(swAggregL, swEdge)
+                        lastSW += 1
 
                         # Hosts loop
-                        for k in irange (1, fanout1):
-				if j % 2 == 1:
-	                                host = topo.addHost('h%s' % lastHost)
-        	                        topo.addLink(swEdge, host)
-                	                lastHost += 1
+                        for k in irange (1, fanout):
+                                host = topo.addHost('h%s' % lastHost)
+                                topo.addLink(swEdge, host)
+                                lastHost += 1
 	
 	return topo
 
